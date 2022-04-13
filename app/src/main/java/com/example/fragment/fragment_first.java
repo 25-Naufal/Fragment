bc81573f267651c7f3989a4636f7a32b1f6f110c
25-Naufal <naufal_rafif_29rpl@student.smktelkom-mlg.sch.id>
2022-04-13 10:07:20 +0700

    update

--- app/src/main/java/com/example/fragment/fragment_first.java
+++ app/src/main/java/com/example/fragment/fragment_first.java
@@ -24,32 +24,33 @@ public class fragment_first extends Fragment {
     public static fragment_first newInstance() {
         return new fragment_first();
     }
-    @Override public void onCreate(@Nullable Bundle savedInstanceState) {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 // init ViewModel
         pageViewModel = ViewModelProviders.of(requireActivity()).get(PageViewModel.class);
     }
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false);
     }
-    @Override public void onViewCreated(@NonNull View view, @Nullable
-            Bundle savedInstanceState) {
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        TextInputEditText nameEditText =
-                view.findViewById(R.id.textInputTextName);
+        TextInputEditText nameEditText = view.findViewById(R.id.textInputTextName);
+
 // Add Text Watcher on name input text
         nameEditText.addTextChangedListener(new TextWatcher() {
-            @Override public void beforeTextChanged(CharSequence charSequence, int
-                    i, int i1, int i2) {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
             }
-            @Override public void onTextChanged(CharSequence charSequence, int i,
-                                                int i1, int i2) {
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
                 pageViewModel.setName(charSequence.toString());
             }
-            @Override public void afterTextChanged(Editable editable) {
+            @Override
+            public void afterTextChanged(Editable editable) {
             }
         });
     }
--- app/src/main/java/com/example/fragment/fragment_second.java
+++ app/src/main/java/com/example/fragment/fragment_second.java
@@ -28,8 +28,7 @@ import androidx.lifecycle.ViewModelProviders;
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 // initialise ViewModel here
-        pageViewModel =
-                ViewModelProviders.of(requireActivity()).get(PageViewModel.class);
+        pageViewModel = ViewModelProviders.of(requireActivity()).get(PageViewModel.class);
     }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -38,16 +37,12 @@ import androidx.lifecycle.ViewModelProviders;
         return inflater.inflate(R.layout.fragment_second, container, false);
     }
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle
-            savedInstanceState) {
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         txtName = view.findViewById(R.id.textViewName);
-        pageViewModel.getName().observe(requireActivity(), new
-                Observer<String>() {
+        pageViewModel.getName().observe(requireActivity(), new Observer<String>() {
                     @Override
-                    public void onChanged(@Nullable String s) {
-                        txtName.setText(s);
-                    }
+                    public void onChanged(@Nullable String s) { txtName.setText(s); }
                 });
     }
 }
